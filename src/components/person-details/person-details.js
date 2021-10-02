2645434914b23103ea0f604dba3a3b8b5e94c2ea
Ponomareva Daria <dorothinein@gmail.com>
2021-10-02 16:25:21 +0300

    ok

---
+++ src/components/person-details/person-details.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import './person-details.css';
+
+export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+  
+  state= {
+    person: null,
+  }
+
+  componentDidMount(){
+    this.upDatePerson();
+  }
+  componentDidUpdate(prevProps){
+    if(this.props.selectedId !== prevProps){
+      this.upDatePerson();
+    }
+    
+  }
+  
+  upDatePerson(){
+    const {selectedId} = this.props;
+    if(!selectedId){
+      return;
+    }
+    this.swapiService
+    .getPerson(selectedId)
+    .then(person=>{
+      this.setState({
+        person,
+      })
+    })
+  }
+  render() {
+    if(!this.state.person){
+      return <span>Select person</span>
+    }
+    const {name, id, gender, birthYear,eyeColor} = this.state.person
+    return (
+      <div className="person-details card">
+        <img className="person-image"
+        alt="snth"
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Gender</span>
+              <span>{gender}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Birth Year</span>
+              <span>{birthYear}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Eye Color</span>
+              <span>{eyeColor}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const  StarshipCard = ()=>{
+    return(
+        <>
+        <h1>StarWars DB</h1>
+        </>
+    )
+}
+export default StarshipCard;
